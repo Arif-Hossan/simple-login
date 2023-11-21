fd9c829b55e8c48aa1785af836ddf606593fbf23
Arif Hossan <arif.hossan.eu@gmail.com>
2023-11-22 01:54:07 +0600

    sign in and sign out with google

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,50 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from 'firebase/auth';
+import app from './firebase.config';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const googleAuth = getAuth(app);
+  const googleProvider = new GoogleAuthProvider();
+  const [user, setUser] = useState();
+  //google sign in
+  const handleGoogleSignIn = () => {
+    signInWithPopup(googleAuth, googleProvider)
+      .then(result => {
+        const loggedUser = result.user;
+        setUser(loggedUser);
+      })
+      .catch(error => {
+        console.log("error", error.message);
+      })
+  }
+  // googleSignOut
+  const handleGoogleSignOut = () => {
+    signOut(googleAuth)
+      .then(() => {
+        console.log("Logged Out")
+        setUser()
+      })
+      .catch(error => {
+        console.log(error.message);
+      })
+
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>{
+        user ? `Hello, ${user.displayName.split(" ")[0]}` : "Hello, buddy"
+      }</h2>
+
+      {/* user ? sign out : sign In */}
+      {
+        user ? <button onClick={handleGoogleSignOut}>Sign Out</button> :
+          <button onClick={handleGoogleSignIn}>Google SignIn</button>
+      }
+
+
     </>
   )
 }
